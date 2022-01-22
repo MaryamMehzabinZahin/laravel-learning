a71327076d2c1801ca9105ab592671f4253d0faa
Maryam Mehzabin Zahin <zahinewu@gmail.com>
2022-01-22 19:40:37 +0600

    update

--- app/Http/Controllers/UserProfile.php
+++ app/Http/Controllers/UserProfile.php
@@ -9,11 +9,11 @@ class UserProfile extends Controller
     function index()
     {
       // return Profile::max('id');
-      $res=new Profile;
-      $res->name="poly";
-      $res->email="pli@gmail.com";
+      $res=Profile::find(2);
+      $res->name="polynew";
+      $res->email="pli9@gmail.com";
       $res->save();
-      
+
       return Profile::all();
      
     }
